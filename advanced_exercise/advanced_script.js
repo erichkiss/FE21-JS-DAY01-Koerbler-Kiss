cea420a8db7802b95a34ca8b9a00c527ae09bccb
Bettina Koerbler <bettina.koerbler@gmail.com>
2021-03-03 14:29:27 +0100

    completed E1 and 2

---
+++ advanced_exercise/advanced_script.js
@@ -0,0 +1,43 @@
+// Advanced Exercise 1 
+var a = 7;
+
+var b = 14;
+
+var c = "21";
+
+var d = '36';
+
+var e = 42;
+
+var sum = 0;
+
+c= Number(c); 
+d= Number(d);
+
+sum = a+b+c+d+e;
+console.log(sum);
+
+
+var f = '1';
+
+var g = 15;
+
+var h = 8;
+
+var i = "1";
+
+var result = 0;
+
+f = Number(f)
+i = Number(i);
+
+result= f*g*h*i;
+console.log(result);
+
+document.write(result/sum);
+
+// Advanced Exercise 2
+
+var multiDim = [ [1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 25, 7], [21, 4, 6, 17], [3, 5, 26, 3] ];
+console.log(multiDim[1][1], multiDim[4][2], multiDim[5][3], multiDim[2][3], multiDim[2][1]);
+
