ffc5316a3fda526571cf68a56153a3eda8f808d3
Bettina Koerbler <bettina.koerbler@gmail.com>
2021-03-03 16:06:35 +0100

    extra excercise

---
+++ extra/script.js
@@ -0,0 +1,9 @@
+// extra 1
+var text = "Hey there, i am a javascript developer, and i live in vienna"
+document.write("I" + text.slice(12, 38) + ".")
+
+// extra 2
+var start = ["apple", "banana", "kiwi"];
+start.splice(2,0, "orange", "strawberry")
+console.log(start);
+
