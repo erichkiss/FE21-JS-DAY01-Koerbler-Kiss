ad71acf910190e7e77595258fc8729f598bb2fd9
Bettina Koerbler <bettina.koerbler@gmail.com>
2021-03-03 14:29:45 +0100

    completed E1 and 2

--- intermediate_exercise/intermediate_script.js
+++ intermediate_exercise/intermediate_script.js
@@ -1,37 +1,18 @@
-// variables for Exercise 1 and 2
 var cars = ['Tesla', 'Audi', 'Renault', 'Volvo', 'Madza', 'Fiat', 'Ferrari'];
-var fruits = ['Apple', 'Banana', 'Kiwi'];
-var animals = ['Monkey', 'Horse', 'Dog'];
-
-
-// variables for Exercise 3
-var fruits2 = ("mango/cherries/kiwi/grapes/pear/peach/orange/lemon");
-var fruit_output = new Array();
-
-
-
 cars.sort();
 
+var fruits = ['Apple', 'Banana', 'Kiwi'];
 fruits.push('Orange');
 console.log(fruits);
 
 fruits.pop();
 console.log(fruits);
 
-
+var animals = ['Monkey', 'Horse', 'Dog'];
 console.log(animals);
 
-animals.unshift('Cat');
+animals.unshift('Cat')
 console.log(animals);
 
-animals.shift();
+animals.shift()
 console.log(animals);
-
-
-
-fruit_output = fruits2.split('/');
-// document.write(fruit_ouput[0]);
-
-document.write(fruit_output.join(" <br> <br> "));
-
-
