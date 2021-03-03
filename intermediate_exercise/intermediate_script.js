222d4d0a515760e64f68bac16bf3115269523e06
Bettina Koerbler <bettina.koerbler@gmail.com>
2021-03-03 12:04:20 +0100

    finished ex 2+3

---
+++ intermediate_exercise/intermediate_script.js
@@ -0,0 +1,18 @@
+var cars = ['Tesla', 'Audi', 'Renault', 'Volvo', 'Madza', 'Fiat', 'Ferrari'];
+cars.sort();
+
+var fruits = ['Apple', 'Banana', 'Kiwi'];
+fruits.push('Orange');
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+var animals = ['Monkey', 'Horse', 'Dog'];
+console.log(animals);
+
+animals.unshift('Cat')
+console.log(animals);
+
+animals.shift()
+console.log(animals);
