076373bea4119a3e25b87d0591446e640c47237e
Bettina Koerbler <bettina.koerbler@gmail.com>
2021-03-03 11:32:21 +0100

    fixed CS array

--- basic_exercise/script.js
+++ basic_exercise/script.js
@@ -2,5 +2,7 @@ var age = 30;
 
 console.log("Hi my name is Martin and I am " +age+ " years old");
 
-var players = [Martin, Thomas, Peter, Mathias, Niki]
+var players = ['Martin', 'Thomas', 'Peter', 'Mathias', 'Niki'];
 console.log("The most valuable player of the match is " +players[2]);
+
+
